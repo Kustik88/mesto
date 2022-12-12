bbab459cc865b884781a6ba81d51350ff14e9684
Kustik88 <Bobrenko88@yandex.ru>
2022-12-12 19:37:10 +0700

    feat: готовность скрипт

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ for (let i = 0; i < likeIcons.length; i++ ) {
 
 editBtn.addEventListener('click', showClosePopUp);
 closeIcon.addEventListener('click', showClosePopUp);
+
+
+let formItem = document.querySelector('.popup__form');
+let nameInput = formItem.querySelector('.popup__owner');
+let jobInput = formItem.querySelector('.popup__job');
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    const newName = nameInput.value;
+    const newJob = jobInput.value;
+    const nameProfile = document.querySelector('.profile__owner');
+    const jobProfile = document.querySelector('.profile__owner-job');
+    nameProfile.textContent = newName;
+    jobProfile.textContent = newJob;
+    showClosePopUp();
+
+}
+
+
+
+formItem.addEventListener('submit', handleFormSubmit);
