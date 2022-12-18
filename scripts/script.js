4392eb6c0c9950afead2ad44bdcb8c4f4bf1304c
Anastasia Bobrenko <Bobrenko88@yandex.ru>
2022-12-18 18:16:53 +0700

    bugfix: устранить замечания ревьюера

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,40 @@
 const editBtn = document.querySelector('.profile__edit-btn');
 const popUp = document.querySelector('.popup');
-const closeIcon = document.querySelector('.popup__close-icon');
+const closeBtn = document.querySelector('.popup__close-btn');
 let likeIcons = document.querySelectorAll('.card__like-icon');
-
-/*закрыть-открыть попап*/
-function showClosePopUp () {
-    popUp.classList.toggle('popup_opened');
-}
-
-/*редактирование профиля через попап*/
 let formItem = popUp.querySelector('.popup__form');
 let nameInput = formItem.querySelector('.popup__owner');
 let jobInput = formItem.querySelector('.popup__job');
+const nameProfile = document.querySelector('.profile__owner');
+const jobProfile = document.querySelector('.profile__job');
+
+function togglePopUp () {
+    if (popUp.classList.contains('.popup_opened') !== true) {
+        nameInput.value = nameProfile.textContent
+        jobInput.value = jobProfile.textContent;
+        popUp.classList.toggle('popup_opened');
+    } else {
+        popUp.classList.toggle('popup_opened');
+    }
+}
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
     const newName = nameInput.value;
     const newJob = jobInput.value;
-    const nameProfile = document.querySelector('.profile__owner');
-    const jobProfile = document.querySelector('.profile__job');
     nameProfile.textContent = newName;
     jobProfile.textContent = newJob;
-    showClosePopUp();
+    togglePopUp();
 }
-
-function closePopUpNoChanges(){
-    const nameProfile = document.querySelector('.profile__owner');
-    const jobProfile = document.querySelector('.profile__job');
-    nameInput.value = nameProfile.textContent;
-    jobInput.value = jobProfile.textContent;
-    showClosePopUp();
-}
-
 
 formItem.addEventListener('submit', handleFormSubmit);
-editBtn.addEventListener('click', showClosePopUp);
-closeIcon.addEventListener('click', closePopUpNoChanges);
+editBtn.addEventListener('click', togglePopUp);
+closeBtn.addEventListener('click', togglePopUp);
 
-/*на следующий спринт, активация лайка*/
+/*на следующий спринт, активация лайка
 for (let i = 0; i < likeIcons.length; i++ ) {
     likeIcons[i].addEventListener('click', function() {
         likeIcons[i].classList.toggle('card__like-icon_active');
     });
 }
+*/
