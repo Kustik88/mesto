970dd8198b9732e58e958a1abb39100ebb4ad7dc
Anastasia Bobrenko <Bobrenko88@yandex.ru>
2023-03-15 14:23:34 +0700

    feat: добавить попап редактирования аватара, улучшшить UX

--- src/pages/index.js
+++ src/pages/index.js
@@ -144,7 +144,6 @@ Promise.all([api.getCurrentUser(), api.getCards()])
     userProfile.setUserAvatar(dataUser)
     userProfile.setUserInfo(dataUser)
     cardList.renderItems(dataCards)
-    console.log(dataCards)
   })
   .catch(err => displayError(err))
 
